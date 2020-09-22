4d0b741883f9c92ff32c35128692ecf2769007e7
n1ckDotEXE <admin@nicholasnam.com>
2020-09-22 11:59:10 -0400

    Completed

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
  * YOUR CODE HERE! *
  *******************/
 
+function yell(param) {
+  return param + "!";
+}
 
+function getFirstCharacter(param) {
+  return param[0]
+}
 
+function getLastCharacter(param) {
+  return param[param.length -1]
+}
 
+function getOneCharacter(param1, param2) {
+  return param1[param2];
+}
 
+function getTwoCharacters(param1, param2, param3) {
+  return param1[param2] + param1[param3];
+}
+
+function makeCapitalized(param1) {
+  return param1.toUpperCase();
+}
+
+function yellLouder(param1) {
+  return param1.toUpperCase() + '!!!';
+}
+
+function getInitials(param1) {
+  return param1[0] + '.' + param1[param1.indexOf(' ') + 1] + '.';
+}
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
